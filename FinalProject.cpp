cc5f0af2bdd0d6a63dcce9dca6bec96984ba004f
doal7262 <doal7262@colorado.edu>
2015-05-04 17:03:12 -0600

    Updated FinalProject.cpp
    -Added a math function
    -Added a game function
    -Fixed the main menu to operate a bit better

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -10,22 +10,28 @@ void insertCommands(); // prototype for inserting commands
 commands. This main also has two classes difined, ht is the HashTable an T is the TextAnswer class */
 int main()
 {
+    cout << "J.A.R.V.I.S ONLINE" << endl;
+
     // Declarations
     int input;
     bool quit = false;
     string command;
     //int cmdNumber;
 
+
+    // Class Declarations
     HashTable ht;
     TextAnswer T;
+    Math m;
 
-    displayMenu();
+    //displayMenu();
     insertCommands(); // This inserts all available commands in a HashTable
 
 /* I like to use switches for my inputs, its makes it look a lot cleaner and makes any wrong input
 have a response.*/
     while(quit != true)
     {
+        displayMenu();
         cin >> input;
         // Clear out cin
         cin.clear();
@@ -34,15 +40,15 @@ have a response.*/
         switch (input)
         {
         case 1: // Weather
-            T.Weather();
-            break;
-
-        case 2: // Info
             T.Info();
             break;
 
-        case 3: // Greeting
+        case 2: // Info
             T.Greeting();
+            break;
+
+        case 3: // Greeting
+            T.Weather();
             break;
 
         case 4: // Search
@@ -56,7 +62,15 @@ have a response.*/
             ht.printInventory();
             break;
 
-        case 6: // Quit
+        case 6: // Math
+            m.initMath();
+            break;
+
+        case 7: // Game
+            T.Game();
+            break;
+
+        case 8: // Quit
             cout << "GoodBye" << endl;
             quit = true;
             break;
@@ -77,7 +91,7 @@ have a response.*/
 /*This will appear everytime the menu starts */
 void displayMenu()
 {
-    cout << "J.A.R.V.I.S ONLINE" << endl;
+    cout << "Press 4 to search for a command or 5 for the full list" << endl;
     return;
 }
 
@@ -86,9 +100,12 @@ void insertCommands()
 {
     HashTable ht;
 
-    ht.insertItem("Weather", 1);
-    ht.insertItem("Info", 2);
-    ht.insertItem("Greeting", 3);
+    ht.insertItem("Info", 1);
+    ht.insertItem("Greeting", 2);
+    ht.insertItem("Weather", 3);
     ht.insertItem("Search", 4);
     ht.insertItem("cmdList", 5);
+    ht.insertItem("Math", 6);
+    ht.insertItem("Game", 7);
+    ht.insertItem("Quit", 8);
 }
