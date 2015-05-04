0372b5c1530b679ab019e0a461cf06d4a0305052
doal7262 <doal7262@colorado.edu>
2015-05-04 17:06:28 -0600

    Updated FinalProjectFunctions.cpp
    -Added a feature for math to be done
    -Added a text game to play

--- FinalProjectFunctions.cpp
+++ FinalProjectFunctions.cpp
@@ -53,7 +53,7 @@ void HashTable::insertItem(std::string name, int cmdNumber)
     {
         hashTable[index]->name = name;
         hashTable[index]->cmdNumber = cmdNumber;
-        std::cout << hashTable[index]->cmdNumber << ". " << hashTable[index]->name << std::endl;
+        //std::cout << hashTable[index]->cmdNumber << ". " << hashTable[index]->name << std::endl;
     }
 
     else
@@ -78,13 +78,13 @@ void HashTable::printInventory()
     for(int i = 0; i < tableSize; i++)
     {
         Item * Ptr = hashTable[i];
-        if(hashTable[i]->name != "nothing")
+        if(hashTable[i]->name != "")
             std::cout << "command: " << hashTable[i]->name << std::endl;
         while(Ptr->next != NULL)
         {
             Ptr = Ptr->next;
-            if(Ptr->name != "nothing")
-                std::cout << "command: " << std::endl;
+            if(Ptr->name != "")
+                std::cout << "command: " << hashTable[i]->name << std::endl;
         }
     }
 }
@@ -117,6 +117,142 @@ Item* HashTable::findItem(std::string name)
     std::cout << "not found" << std::endl;
     return NULL; ///if not found return NULL
 }
+
+int Math::initMath()
+{
+    int mathCommand;
+    int num1;
+    int num2;
+    int answer;
+
+    std::cout << "=====Math=====" << std::endl;
+    std::cout << "1. Addition" << std::endl;
+    std::cout << "2. Subtraction" << std::endl;
+    std::cout << "3. Multiplication" << std::endl;
+    std::cout << "4. Division" << std::endl;
+    std::cout << "5. Ratio" << std::endl;
+
+    switch(mathCommand)
+    {
+    case 1:
+        std::cout << "Input two numbers you want to add" << std::cout;
+        std::cin >> num1 >> num2;
+        answer = addition(num1,num2);
+        return answer;
+
+    case 2:
+        std::cout << "Input two numbers you want to subtract" << std::cout;
+        std::cin >> num1 >> num2;
+        answer = subtraction(num1,num2);
+        return answer;
+
+    case 3:
+        std::cout << "Input two numbers you want to multiply" << std::cout;
+        std::cin >> num1 >> num2;
+        answer = multiplication(num1,num2);
+        return answer;
+
+    case 4:
+        std::cout << "Input two numbers you want to divide" << std::cout;
+        std::cin >> num1 >> num2;
+        answer = division(num1,num2);
+        return answer;
+
+    case 5:
+        std::cout << "Input two numbers you want to take a ratio of" << std::cout;
+        std::cin >> num1 >> num2;
+        answer = Ratio(num1,num2);
+        return answer;
+    }
+}
+
+/*
+int Math::Math()
+{
+
+}
+
+int Math::~Math()
+{
+
+}
+*/
+
+int Math::addition(int num1, int num2)
+{
+    int finalVal = num1 + num2;
+    return finalVal;
+}
+
+int Math::subtraction(int num1, int num2)
+{
+    int finalVal = num1 - num2;
+    return finalVal;
+}
+
+int Math::multiplication(int num1, int num2)
+{
+    int finalVal = num1 * num2;
+    return finalVal;
+}
+
+int Math::division(int num1, int num2)
+{
+    int finalVal = num1 / num2;
+    return finalVal;
+}
+
+int Math::Ratio(int num1, int num2)
+{
+    int finalVal = num1 % num2;
+    return finalVal;
+}
+
+void TextAnswer::Game()
+{
+    std::string gameCommand;
+    //bool acceptable_answer = true;
+    std::cout << "Do you want to fight the (d)ragon or (s)torm the castle?" << std::endl;
+    std::cin >> gameCommand;
+
+    if (gameCommand == "d")
+    {
+        std::string gameCommand2;
+        std::cout << "Will you fight the dragon with a (b)ow, (s)word, or (m)ace" << std::endl;
+        std::cin >> gameCommand2;
+
+        if (gameCommand2 == "b")
+        {
+            std::cout << "Dragon incenerated you, game over." << std::endl;
+        }
+
+        else if(gameCommand2 == "s")
+        {
+            std::cout << "You successfully killed the dragon, you win!" << std::endl;
+        }
+
+        else if(gameCommand2 == "m")
+        {
+            std::cout << "Dragon killed you, game over" << std::endl;
+        }
+
+        else if (gameCommand == "a")
+        {
+            std::cout << "You have no army to storm the castle with, you died" << std::endl;
+        }
+
+        else
+        {
+            std::cout << "You entered a wrong command, you lose!"<< std::endl;
+        }
+    }
+
+    else
+    {
+        std::cout << "That command was not valid" << std::endl;
+    }
+}
+
 /* Simple response to input 'Weather' */
 void TextAnswer::Weather()
 {
